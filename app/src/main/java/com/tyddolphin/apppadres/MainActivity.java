571846653063079d8d30d7146fa7f310846e8bb5
mineiko <gianellmilon@gmail.com>
2017-06-01 20:48:01 -0500

    Se agrego: LinearLayout y separaciones en el mapa_fragment

--- app/src/main/java/com/tyddolphin/apppadres/MainActivity.java
+++ app/src/main/java/com/tyddolphin/apppadres/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,14 +24,22 @@ public class MainActivity extends AppCompatActivity {
         Tabs.setupWithViewPager(mViewPager);
 
 
-        setIconoOnTab(0,R.drawable.ic_place_white_24dp);
+        setIconoOnTab(0,R.drawable.ic_place_white_36dp);
+        setIconoOnTab(1,R.drawable.ic_face_white_36dp);
+        setIconoOnTab(2,R.drawable.ic_directions_bus_white_36dp);
+        setIconoOnTab(3,R.drawable.ic_account_box_white_36dp);
     }
 
     private void setIconoOnTab(int tabID, @DrawableRes int drawable){
         TabLayout.Tab tab = Tabs.getTabAt(tabID);
         if (tab != null){
-            tab.setIcon(drawable);
+            ImageView imageView = new ImageView(this);
+            imageView.setImageResource(drawable);
+
+            tab.setCustomView(imageView);
+            // /tab.setIcon(drawable);
         }
+
     }
 
 }
