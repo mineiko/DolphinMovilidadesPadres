2798fd57d29d2f1a918f124cfb645d8398462410
mineiko <gianellmilon@gmail.com>
2017-07-14 04:39:04 -0500

    se agrego: enviar mensaje

--- app/src/main/java/com/tyddolphin/apppadres/FragmentHijos.java
+++ app/src/main/java/com/tyddolphin/apppadres/FragmentHijos.java
@@ -3,24 +3,31 @@ package com.tyddolphin.apppadres;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.FrameLayout;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.tyddolphin.apppadres.signalR.SignalR;
+
+import static com.tyddolphin.apppadres.R.drawable.ic_markunread_black_48dp;
 
 
 public class FragmentHijos extends Fragment {
@@ -69,13 +76,17 @@ public class FragmentHijos extends Fragment {
         //foto.setGravity(Gravity.CENTER);
         foto.setPadding(20,20,20,0);
         //Button button = new Button(getContext());
-        b.setText("No va a ir");
-        b.setTextSize(15);
+        //b.setText("No va a ir");
+        //b.setTextSize(15);
         b.setLayoutParams(ladderParams);
         b.setHeight(80);
         b.setWidth(160);
         b.setPadding(20,0,20,0);
         b.setGravity(Gravity.CENTER);
+        //Drawable d = getResources().getDrawable(R.drawable.ic_markunread_black_48dp);
+        //b.setBackgroundDrawable( ContextCompat.getDrawable(getContext(), R.drawable.ic_markunread_black_48dp));
+        //b.setBackgroundResource( R.drawable.ic_markunread_black_48dp);
+        //b.setBackgroundDrawable(d);
 
         b.setBackgroundColor(ContextCompat.getColor(this.getContext(),R.color.colorbotones2));
         temporal.addView(foto);
@@ -90,6 +101,10 @@ public class FragmentHijos extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+    LayoutInflater li;
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
+    Button btn;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -100,9 +115,63 @@ public class FragmentHijos extends Fragment {
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         //mLinearLayout.setOrientation(LinearLayout.VERTICAL);
 
-        ImageView ladder = new ImageView(getContext());
-        Button button = new Button(getContext());
-        button.setOnClickListener(new ButtonToast(1));
+
+        btn = (Button) view.findViewById(R.id.mensaje);
+        //Drawable d = getResources().getDrawable(R.drawable.ic_markunread_black_48dp);
+        //button.setBackgroundDrawable( ContextCompat.getDrawable(getContext(), R.drawable.ic_markunread_black_48dp));
+        //button.setBackgroundResource( R.drawable.ic_markunread_black_48dp);
+        //button.setBackgroundDrawable(d);
+        li = getLayoutInflater(savedInstanceState);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(getContext());
+                final View mView = li.inflate(R.layout.dialog_hijos_mensaje,null);
+
+                Button BTNEnviar = (Button) mView.findViewById(R.id.btnEnviar);
+                Button BTNCancelar = (Button) mView.findViewById(R.id.btnCancelar);
+
+//                CheckBox Alumno = new CheckBox(getContext());
+//                CheckBox Alumno2 = new CheckBox(getContext());
+//
+//                Alumno.setText("Adriana Luque");
+//                Alumno2.setText("Christian Lopez");
+                mBuilder.setView(mView);
+                final AlertDialog dialog = mBuilder.create();
+                dialog.show();
+                radioGroup = (RadioGroup) mView.findViewById(R.id.TipoMensaje);
+
+                BTNCancelar.setOnClickListener(new  View.OnClickListener(){
+
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(getContext(),"Elección de Mensaje , cancelada", Toast.LENGTH_LONG).show();
+                        dialog.cancel();
+                    }
+                });
+
+                BTNEnviar.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+
+                        // get selected radio button from radioGroup
+                        int selectedId = radioGroup.getCheckedRadioButtonId();
+
+                        // find the radiobutton by returned id
+                        radioButton = (RadioButton) mView.findViewById(selectedId);
+
+                        Toast.makeText(getContext(),"Mensaje: "+ radioButton.getText() + ", enviada...", Toast.LENGTH_SHORT).show();
+                        dialog.cancel();
+
+                    }
+
+                });
+                //SignalR.enviarMensaje("Holiiii");
+            }
+        });
         /*ladder.setImageResource(R.drawable.b1);
         FrameLayout.LayoutParams ladderParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT,
                 FrameLayout.LayoutParams.WRAP_CONTENT);
@@ -130,8 +199,8 @@ public class FragmentHijos extends Fragment {
         //mLinearLayout.addView(textView);
         //mLinearLayout.addView(button);
 
-        LLODinamica.addView(AgregarHijoLLO("Mateo Paredes",ladder, button));
-        mLinearLayout.addView(LLODinamica);
+        //LLODinamica.addView(AgregarHijoLLO("Mateo Paredes",ladder, button));
+        //mLinearLayout.addView(LLODinamica);
         //LLODinamica.addView(textView);
         //LLODinamica.addView(button);
 
